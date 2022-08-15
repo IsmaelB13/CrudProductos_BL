fefc2ac9f98edf50d0c576eb38b5d21de4c8f9bc
IsmaelB13 <85330514+IsmaelB13@users.noreply.github.com>
2022-08-15 17:44:43 -0500

    Creación del crud

---
+++ ProductosPrueba2/src/productos.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class productos {
+    private JTextField producto;
+    private JTextField precio;
+    private JTextField categoria;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton e;
+    private JTextField ciudad;
+    private JTextField fecha;
+    private JLabel ci;
+}
